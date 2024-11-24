2331828b5810eba0996bcfd20d0e56d4159a5db9
KIZZC <kindtan910@gmail.com>
2024-11-25 00:05:21 +0800

    Update check_bbrx_and_reboot.sh

--- check_bbrx_and_reboot.sh
+++ check_bbrx_and_reboot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 等待系统完全启动
-sleep 60
+sleep 500
 
 # 检查lsmod输出是否包含tcp_bbrx
 if lsmod | grep -q "tcp_bbrx"; then
     echo "tcp_bbrx模块已加载，将在10秒后再次重启系统"
-    sleep 10
+    sleep 15
     reboot
 else
     echo "tcp_bbrx模块未加载，请检查安装和配置"
