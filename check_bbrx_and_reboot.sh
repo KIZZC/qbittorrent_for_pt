205352a109acc15fc06e30c64f9ca33aeffe60d9
KIZZC <kindtan910@gmail.com>
2024-11-25 15:03:07 +0800

    Update check_bbrx_and_reboot.sh

--- check_bbrx_and_reboot.sh
+++ check_bbrx_and_reboot.sh
@@ -2,6 +2,8 @@
 
 # 等待系统完全启动
 sleep 60
+# 替换qbittorrent配置文件
+sudo wget -O /home/admin/.config/qBittorrent/qBittorrent.conf https://raw.githubusercontent.com/KIZZC/qbittorrent_for_pt/refs/heads/main/qBittorrent.conf
 
 # 检查标记文件是否存在，避免循环重启
 if [ ! -f "/var/run/rebooted_once" ]; then
